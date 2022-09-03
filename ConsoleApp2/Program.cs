4cb320d8c39cc8aa2dcb505f837ebaa902ceda32
ar4487 <as3029867@gmail.com>
2022-09-03 20:26:06 +0530

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            int c = b%2 ;
+            Console.WriteLine(c);
+            if (a > 7)
+            {
+                Console.WriteLine("a is greater than 7");
+            }
+            else
+            {
+                Console.WriteLine("7 is greater than a");
+            }
+            Console.WriteLine("please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("value entered by you is " + name);
+        }
+    }
+}
