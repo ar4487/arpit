0c7b1fd2cda5d835f919c0ba6fda57354b5c05de
ar4487 <as3029867@gmail.com>
2022-09-03 19:57:59 +0530

    library

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class SwitchCase
+    {
+        static void Main2(string[] args) { 
+        
+        Console.WriteLine("enter a day");
+        String day = Console.ReadLine();
+            switch (day)
+            {
+                case "monday" :
+                case "tuesday" :
+                case "wednesday" :
+                case "thrusday":
+                case "friday":
+                    Console.WriteLine("it's working day");
+                    break;
+                case "saturday":
+                case "sunday":
+                    Console.WriteLine("it's holiday");
+                    break;
+                default:
+                    Console.WriteLine("plz enter correct date:");
+                    break;
+ 
+
+            }
+            }
+}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("enter your age:");
+           age= Convert.ToInt32(Console.ReadLine());
+     if (age >=60)
+            {
+                Console.WriteLine("you are senior and eligible to vote");
+            }
+     else if (age >=18) 
+            {
+                Console.WriteLine("you are eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("you are not elegible to vote");
+            }
+        }
+    }
+}
